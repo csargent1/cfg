f90e7071bb996f5a65194d336d39944425799371
csargent1 <92318357+csargent1@users.noreply.github.com>
2022-01-26 19:39:58 +0000

    Add files via upload

---
+++ project/products/products.js
@@ -0,0 +1,20 @@
+var newParagraph = document.createElement('p');
+newParagraph.innerText ='I have just been created in JavaScript';
+document.body.appendChild(newParagraph);
+
+newParagraph.style.font = '18px';
+newParagraph.style.font = 'sans-serif';
+newParagraph.style.width = '100px';
+newParagraph.style.border = '1px solid orange'
+newParagraph.style.padding = '30px';
+
+newParagraph.addEventListener('click',function(event){
+    console.log(event);
+    console.log(event.target);
+    event.target.style.color = 'pink';
+
+
+})
+hihihihiihi
+
+
